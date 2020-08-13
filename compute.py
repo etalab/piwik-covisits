0ab156101c35ebee21db8f66bd8c5c8b2d241d49
tk <tk@renard.ninja>
2020-08-13 15:33:14 +0200

    adds a normalized score value
    covisits(e0, e1) / total visits(e0)

--- compute.py
+++ compute.py
@@ -1,32 +1,39 @@
 import networkx as nx
 import glob
 import json
-from itertools import combinations
+#from itertools import combinations
+from itertools import permutations
 
 # agregate
 def build_graph():
-    
+
     g = nx.Graph()
-    
+
     for day in glob.glob('visits/*'):
         print(day)
 
         visits = json.load(open(day, 'r'))
-        
-        covisits = [ list(combinations(urls, 2)) for urls in visits ]
+
+        covisits = [ list(permutations(urls, 2)) for urls in visits ]
 
         for edges in covisits:
             for e in edges:
                 if g.has_edge(e[0], e[1]):
-                    g[e[0]][e[1]]['covisits'] += 1
+                    g[e[1]][e[0]]['covisits'] += 1
                 else:
                     g.add_edge(e[0], e[1], covisits=1)
 
+                if 'links' in g.nodes[e[0]]:
+                    g.nodes[e[0]]['links'] += 1
+                else:
+                    g.nodes[e[0]]['links'] = 1
+
     return g
+
 
 g = build_graph()
 
 print(len(g.nodes))
-print(len(g.edges)) 
+print(len(g.edges))
 
 nx.write_gexf(g, 'covisits.gexf')
--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import json
 
 def label(n):
-    return n.split('/')[-2] 
+    return n.split('/')[-2]
 
 def clean_label(g):
     g = nx.relabel_nodes(g, { n :  n.split('/')[-2] for n in g })
@@ -16,7 +16,7 @@ slug_id = json.load(open('slug_id.json', 'r'))
 def mapping_slug_id(slug):
     return slug_id[slug] if slug in slug_id.keys() else slug
 
-def rule_of_thumb(mylist): 
+def rule_of_thumb(mylist):
     '''Compute rule of thumb on a list
     eg: [100, 80, 20, 14, 12, 11, 1]  => [100, 80] (return the index where to cut)
 
@@ -40,13 +40,13 @@ def filter_covisits(n):
     # print([ edge for edge in g[n] ])
 
     edges = sorted(list(g[n]), key=lambda e: -g[n][e]['covisits'])
-    
+
     # keep only top 10
     edges = edges[:10]
 
     # reduce with rule of thumb
     edges = edges[:rule_of_thumb([ g[n][n2]['covisits'] for n2 in edges ])]
-    
+
     #for n2 in edges:
     #    print("    " + n2 + " - "+ str(g[n][n2]['covisits']))
 
@@ -57,7 +57,7 @@ top50 = json.load(open('top50.json','r'))
 g = nx.read_gexf('covisits.gexf')
 #g = clean_label(g)
 
-n0 ='https://www.data.gouv.fr/fr/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/' 
+n0 ='https://www.data.gouv.fr/fr/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/'
 
 #print(filter_covisits(n0))
 
@@ -76,7 +76,14 @@ for n in tqdm(list(g.nbunch_iter( [ "https://www.data.gouv.fr/fr/datasets/"+l+"/
     data = {
         "slug": label(n),
         "id": mapping_slug_id(label(n)),
-        "recommendations": [ { "slug": label(n2), "id": mapping_slug_id(label(n2)), "score": g[n][n2]['covisits'] } for n2 in edges ]
+        "recommendations": [
+            {
+                "slug": label(n2),
+                "id": mapping_slug_id(label(n2)),
+                "score": g[n][n2]['covisits'],
+                "score_norm": g[n][n2]['covisits']/g.nodes[n]['links']
+            } for n2 in edges
+        ]
     }
 
     print(data)
@@ -85,7 +92,7 @@ for n in tqdm(list(g.nbunch_iter( [ "https://www.data.gouv.fr/fr/datasets/"+l+"/
         json.dump(data, open('scores/'+label(n)+".json", "w"), indent=4, separators=(',', ': '))
     except:
         print('ERROR')
-    
+
     top50recos.append(data)
 
 json.dump(top50recos, open('prod/all.json', "w"), indent=4, separators=(',', ': '))
